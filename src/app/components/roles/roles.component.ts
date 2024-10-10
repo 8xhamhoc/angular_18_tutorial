3c51e3ac3d26b4dd8d07a3926e0a5047bade8e05
Phan Thiện Quang <tomatoquang@gmail.com>
2024-10-10 07:03:45 +0700

    Http client request.

--- src/app/components/roles/roles.component.ts
+++ src/app/components/roles/roles.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { IRole } from '../../model/interface/role';
 
 @Component({
   selector: 'app-roles',
@@ -8,22 +10,44 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.css'
 })
-export class RolesComponent {
-  firstName: string = "Angular tutorial";
-  angularVersion: string = "Version 18";
+export class RolesComponent implements OnInit {
+  
+  roles: IRole[] = [];
+  http: HttpClient = inject(HttpClient);
 
-  version: number = 18;
-  isActive: boolean = false;
-  currentDate: Date = new Date();
-  inputType: string = "button";
-  selectedState: string = '';
+  // constructor(private http: HttpClient) {
 
-  showWelcomeAlert() {
-    alert("Welcome to Angular 18 Tutorial");
+  // }
+
+  ngOnInit(): void {
+    this.getAllRoles();
   }
 
-  showMessage(message: string) {
-    alert(message);
+  getAllRoles() {
+    this.http.get("https://freeapi.miniprojectideas.com/api/ClientStrive/GetAllRoles")
+    .subscribe((res:any) => {
+      this.roles = res.data;
+    });
   }
+
+
+
+
+  // firstName: string = "Angular tutorial";
+  // angularVersion: string = "Version 18";
+
+  // version: number = 18;
+  // isActive: boolean = false;
+  // currentDate: Date = new Date();
+  // inputType: string = "button";
+  // selectedState: string = '';
+
+  // showWelcomeAlert() {
+  //   alert("Welcome to Angular 18 Tutorial");
+  // }
+
+  // showMessage(message: string) {
+  //   alert(message);
+  // }
 
 }
